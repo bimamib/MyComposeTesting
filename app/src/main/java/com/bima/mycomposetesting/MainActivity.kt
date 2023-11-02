8544a83adfbdcff6a6b44340a9fee8d3a2a7ee5b
bimamib <bimaprast1601@gmail.com>
2023-11-02 17:01:48 +0700

    menyesuaikan kode pada MainActivity

--- app/src/main/java/com/bima/mycomposetesting/MainActivity.kt
+++ app/src/main/java/com/bima/mycomposetesting/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.bima.mycomposetesting.ui.CalculatorApp
 import com.bima.mycomposetesting.ui.theme.MyComposeTestingTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,15 +23,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    CalculatorApp()
                 }
             }
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyComposeTestingTheme {
     }
 }
